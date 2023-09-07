093a4722a2d8e9e12504008331757ae401bafdb5
Rikuto <orikutobasuke@gmail.com>
2023-09-07 17:49:15 +0900

    React

--- src/components/sample/02/Component.tsx
+++ src/components/sample/02/Component.tsx
@@ -1,7 +1,11 @@
+import style from '@/styles/Components/sample/02/component.module.scss'
+
 export function Sample02Component() {
     return (
         <>
+            <div className={style.compoWrap}>
 
+            </div>
         </>
     )
 }
