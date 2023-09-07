8d1a9813367f45b9919df5dae77c41ee976567a2
Rikuto <orikutobasuke@gmail.com>
2023-09-07 17:54:36 +0900

    React

--- src/components/sample/02/Component.tsx
+++ src/components/sample/02/Component.tsx
@@ -1,10 +1,20 @@
 import style from '@/styles/Components/sample/02/component.module.scss'
+import { useState } from 'react'
 
-export function Sample02Component() {
+type State = {
+    text: string
+}
+
+export function Sample02Component(props: State) {
+
+    const [state, setState] = useState({
+        text: props.text
+    })
+
     return (
         <>
             <div className={style.compoWrap}>
-
+                <p>{state.text}</p>
             </div>
         </>
     )
--- src/pages/sample/02/index.tsx
+++ src/pages/sample/02/index.tsx
@@ -1,7 +1,21 @@
+import { Sample02Component } from '@/components/sample/02/Component'
+import { useState } from 'react'
+
 export default function Sample02() {
+
+    const [state, setState] = useState({
+        text: ''
+    })
+
     return (
         <>
-
+            <input type="text"
+                onChange={(e) => setState({
+                    ...state,
+                    text: e.target.value
+                })}
+            />
+            <Sample02Component text={state.text} />
         </>
     )
 }
