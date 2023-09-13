215b8a2d9f1e6d8c2de09ecdaabc681499a3cab0
Rikuto <orikutobasuke@gmail.com>
2023-09-13 16:01:52 +0900

    React

---
+++ src/pages/sample/03/index.tsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react'
+import Header from '@/components/template/Header'
+import style from '@/styles/sample/03/index.module.scss'
+
+export default function Sample03() {
+    return (
+        <>
+            <div className={style.wrap}>
+
+            </div>
+        </>
+    )
+}
