a04e756a0549d9ea1ce7de7a391ff6298d0d962d
Rikuto <orikutobasuke@gmail.com>
2023-09-14 13:35:48 +0900

    React

--- src/pages/sample/03/index.tsx
+++ src/pages/sample/03/index.tsx
@@ -2,11 +2,32 @@ import { useState, useEffect } from 'react'
 import Header from '@/components/template/Header'
 import style from '@/styles/sample/03/index.module.scss'
 
+
+function rollDice() {
+    return Math.floor(Math.random() * 6 + 1)
+}
+
 export default function Sample03() {
+
+    const [state, setState] = useState(0)
+
+
+    useEffect(() => {
+        setState(rollDice())
+    }, [])
+
+
     return (
         <>
+            <Header title={'sample03 useEffectについて'} />
             <div className={style.wrap}>
-
+                <div className={style.btnBox}>
+                    <p className={style.text}>{state}</p>
+                    <button
+                        className={style.btn}
+                        onClick={() => setState(rollDice())}
+                    >サイコロをふる</button>
+                </div>
             </div>
         </>
     )
