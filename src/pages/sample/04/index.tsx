7942d030a4241afad51a713e2f96800a49aecb32
Rikuto <orikutobasuke@gmail.com>
2023-09-20 10:49:16 +0900

    時間取得tsファイルを生成

--- src/pages/sample/04/index.tsx
+++ src/pages/sample/04/index.tsx
@@ -1,17 +1,5 @@
-import { useState } from 'react'
-import style from '@/styles/sample/04/index.module.scss'
-
 export default function Sample04() {
-
-    const [state, setState] = useState(0)
-
     return (
-        <>
-            <div className={style.wrap}>
-                <div className={style.calc}>
-
-                </div>
-            </div>
-        </>
+        <></>
     )
 }
