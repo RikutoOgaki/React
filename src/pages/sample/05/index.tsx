70cf122ffdfd4046ab16e36f8f03f9071bf2e2b5
Rikuto <orikutobasuke@gmail.com>
2023-09-25 12:15:49 +0900

    sample05ファイルを追加

--- src/pages/sample/05/index.tsx
+++ src/pages/sample/05/index.tsx
@@ -1,5 +1,10 @@
+import { useState, useEffect } from 'react'
+import style from '@/styles/sample/05/index.module.scss'
+
 export default function Sample05() {
     return (
-        <></>
+        <>
+
+        </>
     )
 }
