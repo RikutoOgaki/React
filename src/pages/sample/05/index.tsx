8a7cc635b7f03f8b8ec6f917758ee1035d5316df
Rikuto <orikutobasuke@gmail.com>
2023-10-05 14:04:48 +0900

    計算機完成

--- src/pages/sample/05/index.tsx
+++ src/pages/sample/05/index.tsx
@@ -12,17 +12,27 @@ export default function Sample05() {
     const calcArray = ['+', '-', '/', '*', '=', 'c']
 
     // 数字が入力されるクリック関数
-    const handleButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-        console.log(e.target);
+    const handleButtonClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
 
-        // setResult(result + value)
+        const value: string = e.currentTarget.innerText
+
+        setResult(result + value)
     }
 
-    // 四則演算用の関数
+    // イコールで計算して、出力する関数
+    const handleEqualClick = () => {
+        if (result.length >= 0) {
+            setResult(eval(result))
+        }
+        else {
+            console.log('数値が入力されてないです');
+        }
+    }
 
     // 数字をクリアする関数
-
-    // イコールで計算して、出力する関数
+    const handleClearClick = () => {
+        setResult('')
+    }
 
     return (
         <>
@@ -43,7 +53,7 @@ export default function Sample05() {
                             <button
                                 key={idx2}
                                 className={style.btn}
-                                onClick={(e) => handleButtonClick(e)}
+                                onClick={(e) => v === '=' ? handleEqualClick() : v === 'c' ? handleClearClick() : handleButtonClick(e)}
                             >{v}</button>
                         )}
                     </div>
