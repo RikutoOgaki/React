a96cb830c1f04d77186e5e27e484740c005348e3
Rikuto <orikutobasuke@gmail.com>
2023-10-11 11:01:12 +0900

    sample09を追加

---
+++ src/pages/sample/09/index.tsx
@@ -0,0 +1,11 @@
+import style from '@/styles/sample/09/index.module.scss'
+
+export default function Sample09() {
+    return (
+        <>
+            <div>
+
+            </div>
+        </>
+    )
+}
