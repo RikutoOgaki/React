94a1e039ef242c0b055db671e585129caaec332e
Rikuto <orikutobasuke@gmail.com>
2023-10-14 01:13:05 +0900

    sample09教えるようで作った内容。後で書き換え

--- src/pages/sample/09/index.tsx
+++ src/pages/sample/09/index.tsx
@@ -1,11 +1,25 @@
+import { useState } from 'react'
 import style from '@/styles/sample/09/index.module.scss'
 
 export default function Sample09() {
+
+    const [state, setState] = useState({
+        data: '',
+        data2: ''
+    })
+
     return (
         <>
-            <div>
+            <div style={{ width: '100%', height: '100vh', backgroundColor: `${state}` }}></div>
 
-            </div>
+            <input type="text" value={state.data}
+                onChange={(e) => {
+                    setState({
+                        ...state,
+                        data: e.target.value
+                    })
+                }}
+            />
         </>
     )
 }
