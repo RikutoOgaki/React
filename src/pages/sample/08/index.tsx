21299d8e163ea4b7badd377274e89c3b0fe056d8
Rikuto <orikutobasuke@gmail.com>
2023-10-13 14:56:17 +0900

    sample08を編集中

--- src/pages/sample/08/index.tsx
+++ src/pages/sample/08/index.tsx
@@ -4,13 +4,13 @@ import style from '@/styles/sample/08/index.module.scss'
 type State = {
 }
 
-// APIについて
-
 export default function Sample08() {
 
     return (
         <>
+            <div className={style.wrap}>
 
+            </div>
         </>
     )
 }
