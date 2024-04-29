7bfb1b0f869082a8b762ccc67ed25dc9ab6d2d07
Rikuto <orikutobasuke@gmail.com>
2024-04-30 00:41:07 +0900

    オリジナルパーティクル作成

---
+++ src/pages/sample/20/index.tsx
@@ -0,0 +1,13 @@
+import style from '@/styles/sample/20/index.module.scss'
+
+export default function Sample20() {
+    return (
+        <>
+            <div className={style.wrap}>
+                <div className={style.particle1}></div>
+                <div className={style.particle2}></div>
+                <div className={style.particle3}></div>
+            </div>
+        </>
+    )
+}
